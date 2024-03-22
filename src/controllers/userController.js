264c159c6c2740e40afc14c4f3d4708bede31b4f
Danish451 <danishkhan229@gmail.com>
2024-03-23 01:07:38 +0530

    update forgot and reset password api

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,7 @@ const User = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { sendVerificationMail } = require("../utils/sendVerificationMail");
+const { sendResetPasswordMail } = require("../utils/sendResetPasswordMail");
 
 async function userRegistration(req, res) {
   const { username, email, password, isVerified } = req.body;
@@ -115,87 +116,69 @@ async function verifyMail(req, res) {
     console.log(error.message);
   }
 }
-async function sendUserPasswordResetEmail(req, res) {
-  const { email } = req.body
-  if(email){
-    if(user) {
-      const user = await User.findOne({ email: email })
 
-      if(user) {
-        const secret = user._id + process.env.jwtSecret
-        const token = jwt.sign({ userId: user._id }, secret, {
-          expiresIn: '15m'
-        })
-        const link = `http://localhost:3000/api/user/reset/${user._id}/${token}`
-
-        res.send({ "status" : "success", "message" : "Password Reset Email Sent... Please Check Your Email" })
-      } else {
-          res.status(401).json({
-            status: "failed", 
-            message: "Email doesn't exists" 
-          });  
-      }
-    } else {
-      res.status(401).json({
-        status: "failed", 
-        message: "Email doesn't exists" 
-      });
-    }
-  } else {
-    res.status(400).json({
-      status: "failed",
-      message: "Email field is required" 
-    });
-  }
   async function forgotPassword(req, res) {
-    const { email } = req.body;
-    try {
-      if (!email) {
-        return res.status(400).json({ status: "failed", message: "Email field is required" });
-      }
-  
-      const user = await User.findOne({ email });
-      if (!user) {
-        return res.status(404).json({ status: "failed", message: "Email not found" });
-      }
-  
-      const secret = user._id + process.env.jwtSecret;
-      const token = jwt.sign({ userId: user._id }, secret, { expiresIn: '15m' });
-      const link = `http://localhost:3000/api/user/reset/${user._id}/${token}`;
-
-      res.status(200).json({ status: "success", message: "Password Reset Email Sent... Please Check Your Email" });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ status: "failed", message: "Unable to process request" });
-      }
+  const { email } = req.body;
+  try {
+    if (!email) {
+      return res.status(400).json({ status: "failed", message: "Email field is required" });
     }
-  async function userPasswordReset(req, res) {
-    const { password } = req.body
-    const { id, token } = req.params
-    const user = await User.findById(id)
 
-    const new_secret = user._id + process.env.jwtSecret
-
-    try {
-      jwt.verify(token, new_secret)
-      const salt = await bcrypt.genSalt(10)
-      const newHashPassword = await bcrypt.hash(password, salt)
-      await User.findByIdAndUpdate(user._id, { $set: { password: newHashPassword }})
-
-      res.send({ status: "success", message: "Password Reset Successfully "})
-    } catch (error) {
-      res.status(401).json({
-        status: "failed", 
-        message: "Invalid Token" 
-      });
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ status: "failed", message: "Email not found" });
     }
+
+    const secret = user._id + process.env.jwtSecret;
+    const token = jwt.sign({ userId: user._id }, secret, { expiresIn: '15m' });
+    const link = `http://localhost:3000/api/user/reset/${user._id}/${token}`;
+    console.log(link);
+
+    sendResetPasswordMail(user.username, email, link); // Assuming sendResetPasswordMail is defined elsewhere
+    res.status(200).json({ status: "success", message: "Password Reset Email Sent... Please Check Your Email" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: "failed", message: "Unable to process request" });
   }
 }
+
+async function userPasswordReset(req, res) {
+  const { password, password_confirmation } = req.body
+  const { id, token } = req.params
+  const user = await User.findById(id)
+
+  const new_secret = user._id + process.env.jwtSecret
+
+  try {
+    jwt.verify(token, new_secret)
+    if(password && password_confirmation) {
+      if(password !== password_confirmation) {
+        res.status(401).json({
+          status: "failed", 
+          message: "New Password and Confirm New Password doesn't match" 
+        });
+      } else {
+        const salt = await bcrypt.genSalt(10)
+        const newHashPassword = await bcrypt.hash(password, salt)
+        await User.findByIdAndUpdate(user._id, { $set: { password: newHashPassword }})
+
+        res.send({ status: "success", message: "Password Reset Successfully "})
+      }
+    } else{
+      res.status(400).json({ status: "failed", message: "Email field is required" });
+    }
+     } catch (error) {
+    res.status(401).json({
+      status: "failed", 
+      message: "Invalid Token" 
+    });
+  }
+}
+
 module.exports = { 
   userRegistration, 
   userLogin, 
   verifyMail, 
-  sendUserPasswordResetEmail, 
   forgotPassword, 
   userPasswordReset 
 };
