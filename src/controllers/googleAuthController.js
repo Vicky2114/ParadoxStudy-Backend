100fad6b44da5979acb0d8342122b79b90ee11c4
Ishika patni <132484010+ishika3838@users.noreply.github.com>
2024-10-06 03:19:45 -0700

    google auth

---
+++ src/controllers/googleAuthController.js
@@ -0,0 +1,47 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const dotenv=require('dotenv')
+//const User = require('../models/user'); 
+const User=require('../models/user.model')// Adjust the path to your User model
+dotenv.config();
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:8000/api/user/google/callback" // Use your actual redirect URL
+},
+async (accessToken, refreshToken, profile, done) => {
+  // Find or create user logic
+  try {
+    let user = await User.findOne({ googleId: profile.id });
+
+    if (!user) {
+      user = new User({
+        googleId: profile.id,
+        name: profile.displayName,
+        email: profile.emails[0].value,
+        avatar: profile.photos[0].value
+      });
+      await user.save();
+    }
+    
+    return done(null, user);
+  } catch (err) {
+    return done(err, null);
+  }
+}));
+
+// Serialize user into session
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// Deserialize user from session
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
+module.exports = passport;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,4 +3,5 @@ const router = express.Router();
 
 router.use("/user", require("./userRoutes"));
 
+
 module.exports = router;
