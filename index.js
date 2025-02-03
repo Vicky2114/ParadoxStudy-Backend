c2b62593eebc5cefc966ae76f71c29f2e8f30b45
Vishal Rawat <vr6994056@gmail.com>
2025-02-03 09:36:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ mongoose
   })
   .then(() => {
     console.log("Connected to MongoDB");
-    const port = 8080;
+    const port = 8000;
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
