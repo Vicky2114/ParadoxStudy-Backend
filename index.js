27ebe565bb195c9a98b20169cc433497f708ca69
Vishal Rawat <vr6994056@gmail.com>
2025-02-03 08:57:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,6 @@ mongoose
   })
   .then(() => {
     console.log("Connected to MongoDB");
-    // Start the server after successful connection
     const port = 8080;
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
